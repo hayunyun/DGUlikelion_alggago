1e7ef654f2cc8e41af12367e396ea5c69cb995a7
hayunyun <hayunyun@naver.com>
2020-10-16 00:38:08 +0900

    final

--- py_ai_DGU.py
+++ py_ai_DGU.py
@@ -10,45 +10,45 @@ with open('temp.json') as json_file:
 MAX_NUMBER = 16000
 
 
-my_position = [] #내 돌의 위치정보(좌표)
-your_position = [] #상대방 돌의 위치정보(좌표)
+my_position = [] 
+your_position = [] 
 
 for key in json_data["my_position"].keys():
-    my_position.append(json_data["my_position"][key]) #내 포지션 배열에 돌 좌표 하나씩 기입
+    my_position.append(json_data["my_position"][key]) 
 
 for key in json_data["your_position"].keys():
-    your_position.append(json_data["your_position"][key]) #상대포지션도 마찬가지
+    your_position.append(json_data["your_position"][key]) 
 
-#내가 가지고 있는 돌들 중에서 가장 가까운 위치에 있는 상대방의 돌을 알아내서 공격하는 알고리즘
+
 current_stone_number = 0 
 index = 0
 min_length = MAX_NUMBER #16000
 x_length = MAX_NUMBER
 y_length = MAX_NUMBER
 
-#전략 추가
-moongchim = MAX_NUMBER
+
+two_stone = MAX_NUMBER
 your_dist = MAX_NUMBER
 a = 0 #상대방 돌의 index 번호
-b = 0 #이것도
-moongchim_index1 = 0
-moongchim_index2 = 0
+b = 0 #상대방 돌의 index 번호2
+two_stone_index1 = 0
+two_stone_index2 = 0
 
 #상대방 돌이 얼마나 뭉쳐있는지 확인
 while a <= (len(your_position)-2):
     b = a+1
     while b <= (len(your_position)-1):
         your_dist = math.sqrt((your_position[a][0]-your_position[b][0])*(your_position[a][0]-your_position[b][0])+(your_position[a][1]-your_position[b][1])*(your_position[a][1]-your_position[b][1]))
-        if moongchim > your_dist:
-            moongchim_index1 = a #뭉친돌1
-            moongchim_index2 = b #뭉친돌2
-            moongchim = your_dist
+        if two_stone > your_dist:
+            two_stone_index1 = a #뭉친돌1
+            two_stone_index2 = b #뭉친돌2
+            two_stone = your_dist
         b += 1
     a += 1
 
 #두 돌 사이 공격
-avg_x = (your_position[moongchim_index1][0]+your_position[moongchim_index2][0])/2
-avg_y = (your_position[moongchim_index1][1]+your_position[moongchim_index2][1])/2
+avg_x = (your_position[two_stone_index1][0]+your_position[two_stone_index2][0])/2
+avg_y = (your_position[two_stone_index1][1]+your_position[two_stone_index2][1])/2
 
 #충분히 뭉쳐있다면 뭉친 돌들을 공격, 아니라면 그냥 가장 가까운 돌 공격
 if your_dist <= 150:
@@ -64,9 +64,8 @@ if your_dist <= 150:
             y_length = avg_y - my[1] 
             stone_x_strength = x_length * 10 
             stone_y_strength = y_length * 10
-
-
     index = index + 1
+
 else: #뭉쳐있는 곳이 없는 경우
     for my in my_position:
         for your in your_position:
@@ -80,8 +79,6 @@ else: #뭉쳐있는 곳이 없는 경우
                 y_length = your[1] - my[1] #최소거리에서의 y축거리
                 stone_x_strength = x_length * 7 
                 stone_y_strength = y_length * 7
-
-
         index = index + 1
 
 
@@ -91,5 +88,5 @@ stone_number = current_stone_number #최종 도출된 최소거리에 있는 상
 
 
 
-result = [stone_number, stone_x_strength, stone_y_strength, message, moongchim_index1, moongchim_index2]
+result = [stone_number, stone_x_strength, stone_y_strength, message, two_stone_index1, two_stone_index2]
 print(str(result)[1:-1].replace("'", ""))
